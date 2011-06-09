103d6c9cf526ae652771bd22d79c346746a4652a
Faraaz Sareshwala <fsareshwala@quantcast.com>
2011-06-09 14:07:27 -0700

    awesome: set n and p to switch to the next tag and previous tag

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -170,7 +170,8 @@ clientkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
     awful.key({ modkey, "Shift"   }, "r",      function (c) c:redraw()                       end),
-    awful.key({ modkey,           }, "n",      function (c) c.minimized = not c.minimized    end),
+    awful.key({ modkey,           }, "n",      awful.tag.viewnext                               ),
+    awful.key({ modkey,           }, "p",      awful.tag.viewprev                               ),
     awful.key({ modkey,           }, "m",
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
