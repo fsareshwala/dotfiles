639bfc91009802e222a072d80538649712ed34a4
Faraaz Sareshwala <fsareshwala@quantcast.com>
2014-04-09 19:37:43 -0700

    update_subtree: initial add

---
+++ prefix/bin/update_subtree.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git pull -s subtree https://github.com/tpope/vim-fugitive.git master
+git pull -s subtree https://github.com/scrooloose/nerdtree.git master
+git pull -s subtree https://github.com/jboyens/vim-protobuf.git master
+git pull -s subtree https://github.com/plasticboy/vim-markdown.git master
+git pull -s subtree https://github.com/jnwhiteh/vim-golang.git master
+git pull -s subtree https://github.com/tpope/vim-pathogen.git master
