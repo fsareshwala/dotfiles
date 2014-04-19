63f015fd9f9e7e97fccfcef1091967bc69779e57
Faraaz Sareshwala <fsareshwala@quantcast.com>
2014-04-19 15:02:52 -0700

    puppet: dont create our own service directory but write directly into daemontools

--- prefix/etc/puppet/site.pp
+++ prefix/etc/puppet/site.pp
@@ -4,6 +4,7 @@ package { [
   'daemontools-run',
   'ntp',
   'perl',
+  'offlineimap',
 ]:
   ensure => latest,
 }
@@ -16,48 +17,36 @@ cron { puppet:
 }
 
 cron { offlineimap:
-  require => Package['daemontools'],
   command => '/usr/bin/setlock -nx /tmp/offlineimap.lock offlineimap >> /tmp/offlineimap.cron',
   user => fsareshwala,
   target => fsareshwala,
   hour => '*',
 }
 
-file { '/service':
-  require => Package['daemontools'],
-  ensure => link,
-  target => '/etc/service',
-  owner => root,
-  group => root,
-}
-
 file { [
-  '/home/fsareshwala/prefix',
-  '/home/fsareshwala/prefix/etc',
-  '/home/fsareshwala/prefix/etc/service',
-  '/home/fsareshwala/prefix/etc/service/ntpd',
+  '/etc/service/ntpd',
 ]:
   ensure => directory,
   require => Package['daemontools'],
 }
 
-file { '/home/fsareshwala/prefix/etc/service/ntpd/run':
+file { '/etc/service/ntpd/run':
   ensure => present,
   backup => false,
-  owner => fsareshwala,
-  group => fsareshwala,
-  mode => 700,
-  require => File['/home/fsareshwala/prefix/etc/service/ntpd'],
+  owner => root,
+  group => root,
+  mode => 755,
+  require => File['/etc/service/ntpd'],
   content => '#!/bin/sh
 exec /usr/sbin/ntpd -n -g -u 113:121
 '
 }
 
 service { 'ntpd':
-  require => File['/home/fsareshwala/prefix/etc/service/ntpd/run'],
+  require => File['/etc/service/ntpd/run'],
   provider => 'daemontools',
-  path => '/home/fsareshwala/prefix/etc/service',
+  path => '/etc/service',
   enable => true,
   ensure => running,
-  subscribe => [File['/home/fsareshwala/prefix/etc/service/ntpd/run'], Package['ntp']],
+  subscribe => [File['/etc/service/ntpd/run'], Package['ntp']],
 }
