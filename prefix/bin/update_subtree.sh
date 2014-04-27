94ea98f50c78ec132c7d575de63aaf5336ebec4a
Faraaz Sareshwala <fsareshwala@quantcast.com>
2014-04-26 20:33:18 -0700

    update_subtree: add ack

--- prefix/bin/update_subtree.sh
+++ prefix/bin/update_subtree.sh
@@ -5,3 +5,4 @@ git pull -s subtree https://github.com/jboyens/vim-protobuf.git master
 git pull -s subtree https://github.com/plasticboy/vim-markdown.git master
 git pull -s subtree https://github.com/jnwhiteh/vim-golang.git master
 git pull -s subtree https://github.com/tpope/vim-pathogen.git master
+git pull -s subtree https://github.com/mileszs/ack.vim.git
