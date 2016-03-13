413a686da4392be4751467198392a68740fd7112
Faraaz Sareshwala <fsareshwala@quantcast.com>
2016-03-13 13:21:13 -0700

    prefix/bin/update_subtree: add vim-table-mode

--- prefix/bin/update_subtree.sh
+++ prefix/bin/update_subtree.sh
@@ -8,3 +8,4 @@ git pull -s subtree https://github.com/tpope/vim-pathogen.git master
 git pull -s subtree https://github.com/mileszs/ack.vim.git master
 git pull -s subtree https://github.com/muennich/urxvt-perls.git master
 git pull -s subtree https://github.com/rkitover/vimpager.git master
+git pull -s subtree https://github.com/dhruvasagar/vim-table-mode.git master
