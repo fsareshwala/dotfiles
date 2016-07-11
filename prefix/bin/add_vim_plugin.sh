50f657266a4373870b9aa412cffa8de8d54a34cc
Faraaz Sareshwala <fsareshwala@twitter.com>
2016-07-11 16:47:08 -0700

    prefix: add script to add vim plugins automatically

---
+++ prefix/bin/add_vim_plugin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <name> <url>"
+    exit 1
+fi
+
+name=$1
+url=$2
+
+git remote add -f $name $url
+git merge -s ours --no-commit $name/master
+git read-tree --prefix=.vim/bundle/$name -u $name/master
+git commit -m "vim: subtree merge $name"
