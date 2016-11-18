f87ee7b668b6384ba92027b3ae804bee2a91caeb
Faraaz Sareshwala <fsareshwala@twitter.com>
2016-11-17 20:21:31 -0800

    fish: add abbreviations to local session

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,24 @@ function tunnel
     ssh -N -L 8080:$argv n
 end
 
+if status --is-interactive
+    set -g fish_user_abbreviations
+    abbr --add b git branch
+    abbr --add ba git branch -a
+    abbr --add d git diff
+    abbr --add dc git diff --cached
+    abbr --add ga git add
+    abbr --add gap git add -p
+    abbr --add gca git commit --amend
+    abbr --add gcan git commit --amend --no-edit
+    abbr --add gcb git checkout -b
+    abbr --add gf git fx
+    abbr --add gm git commit -m
+    abbr --add griom git rebase -i origin/master
+    abbr --add grom git rebase origin/master
+    abbr --add st git status
+end
+
 alias bc 'bc -l'
 alias gdb 'cgdb --directory=. -quiet'
 alias make 'make -j 4'
