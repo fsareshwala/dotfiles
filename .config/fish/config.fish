7f14cf4bacb964b5968833bfed2b25355edee189
Faraaz Sareshwala <faraazs@gmail.com>
2017-04-28 16:19:44 -0700

    fish: format configuration file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,50 +12,50 @@ set --export PATH $GOPATH/bin $PATH
 set --export PATH ~/.npm-global/bin $PATH
 
 function install
-    sudo apt install -y $argv
+  sudo apt install -y $argv
 end
 
 function update
-    sudo apt update
+  sudo apt update
 end
 
 function upgrade
-    sudo apt -y upgrade
+  sudo apt -y upgrade
 end
 
 function tunnel
-    ssh -N -L 8080:$argv n
+  ssh -N -L 8080:$argv n
 end
 
 if status --is-interactive
-    set -g fish_user_abbreviations
+  set -g fish_user_abbreviations
 
-    # git abbreviations
-    abbr --add b git branch
-    abbr --add ba git branch -a
-    abbr --add d git diff
-    abbr --add dc git diff --cached
-    abbr --add ga git add
-    abbr --add gap git add -p
-    abbr --add gca git commit --amend
-    abbr --add gcan git commit --amend --no-edit
-    abbr --add gc git checkout -b fsareshwala/
-    abbr --add gf git fx
-    abbr --add gm git commit -m
-    abbr --add griom git rebase -i origin/master
-    abbr --add grom git rebase origin/master
-    abbr --add st git status
+  # git abbreviations
+  abbr --add b git branch
+  abbr --add ba git branch -a
+  abbr --add d git diff
+  abbr --add dc git diff --cached
+  abbr --add ga git add
+  abbr --add gap git add -p
+  abbr --add gca git commit --amend
+  abbr --add gcan git commit --amend --no-edit
+  abbr --add gc git checkout -b fsareshwala/
+  abbr --add gf git fx
+  abbr --add gm git commit -m
+  abbr --add griom git rebase -i origin/master
+  abbr --add grom git rebase origin/master
+  abbr --add st git status
 
-    # twitter
-    abbr --add ppost arc diff --browse
-    abbr --add psubmit arc land
-    abbr --add rpost git review post --use-commit-message
-    abbr --add rsubmit git review submit --skip-show-build --use-commit-message
+  # twitter
+  abbr --add ppost arc diff --browse
+  abbr --add psubmit arc land
+  abbr --add rpost git review post --use-commit-message
+  abbr --add rsubmit git review submit --skip-show-build --use-commit-message
 
-    # miscellaneous
-    abbr --add r tmux attach
-    abbr --add s source ~/.config/fish/config.fish
-    abbr --add fc forecash
+  # miscellaneous
+  abbr --add r tmux attach
+  abbr --add s source ~/.config/fish/config.fish
+  abbr --add fc forecash
 end
 
 alias bc 'bc -lq'
@@ -63,5 +63,6 @@ alias gdb 'cgdb --directory=. -quiet'
 alias pmake 'set cores (grep -c "^processor" /proc/cpuinfo); and make -j $cores'
 alias reswap 'sudo /sbin/swapoff -a; and sudo /sbin/swapon -a'
 alias vim 'vim -O'
+alias watch 'watch --color'
 
 ulimit -c unlimited
