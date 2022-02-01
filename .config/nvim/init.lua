0b9ed9ffd3f17b307d7f15325d804bf4ce1355d4
Faraaz Sareshwala <faraazs@gmail.com>
2022-01-31 23:27:57 -0800

    vim: switch to lua configuration

---
+++ .config/nvim/init.lua
@@ -0,0 +1,538 @@
+local function get_hostname()
+  local f = io.popen ("/bin/hostname")
+  local value = f:read("*a")
+  f:close()
+  return string.gsub(value, '\n$', '')
+end
+
+local function at_work()
+  local hostname = get_hostname()
+
+  if hostname == 'fsareshwala-glaptop' then
+    return true
+  elseif hostname == 'fsareshwala-cloudtop' then
+    return true
+  end
+
+  return false
+end
+
+local function set_options()
+  vim.opt.autoindent = true
+  vim.opt.autoread = true
+  vim.opt.autowriteall = true
+  vim.opt.background = 'dark'
+  vim.opt.backspace = {'eol', 'start', 'indent'}
+  vim.opt.backup = false
+  vim.opt.clipboard = 'unnamedplus'
+  vim.opt.colorcolumn = '+1'
+  vim.opt.copyindent = true
+  vim.opt.expandtab = true
+  vim.opt.exrc = true
+  vim.opt.formatoptions = 'cjlnqrt'
+  vim.opt.gdefault = true
+  vim.opt.hlsearch = true
+  vim.opt.ignorecase = true
+  vim.opt.inccommand = 'split'
+  vim.opt.incsearch = true
+  vim.opt.iskeyword:append '-'
+  vim.opt.joinspaces = false
+  vim.opt.laststatus = 2
+  vim.opt.linebreak = true
+  vim.opt.list = true
+  vim.opt.listchars = {tab = '|-', trail = '-', extends = '>', precedes = '<'}
+  vim.opt.matchtime = 3
+  vim.opt.modeline = true
+  vim.opt.modelines = 5
+  vim.opt.mouse = ''
+  vim.opt.nrformats:append {'alpha', 'octal'}
+  vim.opt.number = true
+  vim.opt.preserveindent = true
+  vim.opt.pumheight = 10
+  vim.opt.shell = '/bin/bash'
+  vim.opt.shiftround = true
+  vim.opt.shiftwidth = 2
+  vim.opt.shortmess:append 'c'
+  vim.opt.showmatch = true
+  vim.opt.showmode = false
+  vim.opt.showtabline = 2
+  vim.opt.signcolumn = 'yes'
+  vim.opt.smartcase = true
+  vim.opt.smarttab = true
+  vim.opt.softtabstop = 2
+  vim.opt.spellfile:append(vim.fn.glob('~/.config/nvim/spell/en.utf-8.add'))
+  vim.opt.spellfile:append(vim.fn.glob('~/personal/vim/en.utf-8.add'))
+  vim.opt.splitbelow = true
+  vim.opt.splitright = true
+  vim.opt.startofline = false
+  vim.opt.swapfile = false
+  vim.opt.tabstop = 2
+  vim.opt.tags = 'tags;'
+  vim.opt.timeoutlen = 500
+  vim.opt.title = true
+  vim.opt.undodir = vim.fn.glob('~/.config/nvim/tmp/undo')
+  vim.opt.undofile = true
+  vim.opt.updatetime = 300
+  vim.opt.virtualedit:append 'block'
+  vim.opt.wildignore:append {'*.so', '*.o'}
+  vim.opt.wildmenu = true
+  vim.opt.wildmode = 'list:longest,full'
+  vim.opt.wrap = true
+  vim.opt.wrapscan = true
+  vim.opt.writebackup = false
+
+  -- integrate builtin lsp with tags
+  vim.cmd [[set tagfunc=v:lua.vim.lsp.tagfunc]]
+
+  vim.g.markdown_fenced_languages = {'python', 'vim', 'cpp', 'java'}
+  vim.g.tex_flavor = 'latex'
+end
+
+local function set_keymaps()
+  local opts = {noremap = true, silent = true}
+  local keymap = vim.api.nvim_set_keymap
+
+  -- local command = 'c'
+  -- local insert = 'i'
+  local normal = 'n'
+  -- local term = 't'
+  local visual = 'v'
+  -- local visual_block = 'x'
+
+  -- better window/tab/split controls
+  keymap(normal, '<c-h>', '<c-w>h', opts)
+  keymap(normal, '<c-j>', '<c-w>j', opts)
+  keymap(normal, '<c-k>', '<c-w>k', opts)
+  keymap(normal, '<c-l>', '<c-w>l', opts)
+  keymap(normal, '<leader>s', ':vsplit<cr>', opts)
+  keymap(normal, '<leader>t', ':tabnew<cr>', opts)
+  keymap(normal, 'H', ':tabprev<cr>', opts)
+  keymap(normal, 'L', ':tabnext<cr>', opts)
+
+  -- resize with arrows
+  keymap(normal, '<c-up>', ':resize +2<cr>', opts)
+  keymap(normal, '<c-down>', ':resize -2<cr>', opts)
+  keymap(normal, '<c-left>', ':vertical resize -2<cr>', opts)
+  keymap(normal, '<c-right>', ':vertical resize +2<cr>', opts)
+
+  -- movement across virtually wrapped lines
+  keymap(normal, '0', 'g0', opts)
+  keymap(normal, '$', 'g$', opts)
+  keymap(normal, 'j', 'gj', opts)
+  keymap(normal, 'k', 'gk', opts)
+
+  -- hold onto indent mode when indenting
+  keymap(visual, '<', '<gv', opts)
+  keymap(visual, '>', '>gv', opts)
+
+  -- insert an empty lines before and after the current line
+  keymap(normal, '<s-enter>', 'O<esc>', opts)
+  keymap(normal, '<enter>', 'o<esc>', opts)
+
+  -- replace current word with contents of paste buffer
+  keymap(normal, '<c-p>', '"_cw"<esc>', opts)
+
+  -- file shortcuts
+  keymap(normal, '<leader>a', '<cmd>ClangdSwitchSourceHeader<cr>', opts)
+  keymap(normal, '<leader>v', '<cmd>e  ~/.config/nvim/init.lua<cr>', opts)
+  keymap(normal, '<leader>wj', '<cmd>e ~/personal/journal.md<cr>', opts)
+  keymap(normal, '<leader>wk', '<cmd>e ~/personal/career/google.md<cr>', opts)
+  keymap(normal, '<leader>wn', '<cmd>e ~/personal/career/notes.md<cr>', opts)
+
+
+  -- stay at current word when using star search
+  keymap(normal, '*', '*<c-o>', opts)
+
+  -- underline current line when only in normal mode
+  keymap(normal, 'U', 'YpVr-', opts)
+
+  -- telescope keymaps
+  keymap(normal, '<leader>e', '<cmd>Telescope find_files<cr>', opts)
+  keymap(normal, '<leader>f', '<cmd>Telescope live_grep<cr>', opts)
+  keymap(normal, '<leader>g', '<cmd>Telescope grep_string<cr>', opts)
+  keymap(normal, '<leader>z', '<cmd>Telescope spell_suggest<cr>', opts)
+  keymap(normal, '<leader>s', '<cmd>Telescope current_buffer_fuzzy_find<cr>', opts)
+
+  -- file tree keymaps
+  keymap(normal, '<leader>n', '<cmd>NvimTreeToggle<cr>', opts)
+  keymap(normal, '<leader>l', '<cmd>NvimTreeFindFile<cr>', opts)
+end
+
+local function install_plugins()
+  local data_path = vim.fn.stdpath('data')
+  local packer_subfile = '/site/pack/packer/start/packer.nvim'
+  local install_path = data_path .. packer_subfile
+  local packer_repo = 'https://github.com/wbthomason/packer.nvim'
+
+  if vim.fn.empty(vim.fn.glob(install_path)) then
+    vim.fn.system({'git', 'clone', packer_repo, install_path})
+    vim.cmd [[packadd packer.nvim]]
+  end
+
+  local packer = require('packer')
+
+  return packer.startup(function(use)
+    use 'wbthomason/packer.nvim'    -- let packer manage itself
+
+    use 'chriskempson/base16-vim' -- colorscheme
+    use 'jiangmiao/auto-pairs'    -- automatically insert/delete parenthesis, brackets, quotes
+    use 'tpope/vim-abolish'       -- {} syntax (:Abolish, :Subvert), case style change (crc)
+    use 'tpope/vim-commentary'    -- motions to comment lines out
+    use 'tpope/vim-fugitive'      -- git bidings
+    use 'tpope/vim-repeat'        -- allow plugins to override .
+    use 'tpope/vim-sleuth'        -- automatically adjust shiftwidth and expandtab
+    use 'tpope/vim-speeddating'   -- ctrl+a and ctrl+x on dates
+    use 'tpope/vim-surround'      -- motions to surround text with other text
+
+    use 'chaoren/vim-wordmotion'  -- better word motions through long strings
+    vim.g.wordmotion_spaces = '_-.'
+
+    -- fuzzy finder over files, commands, lists, etc
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim',
+        {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+      }
+    }
+
+    -- Better syntax highlighting
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+    }
+
+    -- completion engine
+    use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+        'hrsh7th/cmp-buffer',     -- text in current buffer
+        'hrsh7th/cmp-nvim-lua',   -- neovim lua api
+        'hrsh7th/cmp-path'        -- filesystem paths
+      }
+    }
+
+    -- language server protocol
+    use {
+      'neovim/nvim-lspconfig',
+      requires = {
+        'hrsh7th/cmp-nvim-lsp',    -- lsp based completions
+        'williamboman/nvim-lsp-installer'
+      }
+    }
+
+    -- file tree
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {'kyazdani42/nvim-web-devicons'}
+    }
+
+    -- work plugins
+    if at_work() then
+      use '~/code/fuchsia/garnet/public/lib/fidl/tools/vim'
+      use '~/code/emboss/integration/vim/ft-emboss'
+      use {
+        'https://gn.googlesource.com/gn',
+        rtp = 'misc/vim'
+      }
+    end
+  end)
+end
+
+local function setup_completions()
+  local cmp = require('cmp')
+
+  local kind_icons = {
+    Class = '',
+    Color = '',
+    Constant = '',
+    Constructor = '',
+    Enum = '',
+    EnumMember = '',
+    Event = '',
+    Field = '',
+    File = '',
+    Folder = '',
+    Function = '',
+    Interface = '',
+    Keyword = '',
+    Method = 'm',
+    Module = '',
+    Operator = '',
+    Property = '',
+    Reference = '',
+    Snippet = '',
+    Struct = '',
+    Text = '',
+    TypeParameter = '',
+    Unit = '',
+    Value = '',
+    Variable = '',
+  }
+
+  cmp.setup {
+    mapping = {
+      ['<c-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), {'i', 'c'}),
+      ['<c-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), {'i', 'c'}),
+      ['<c-j>'] = cmp.mapping.select_next_item(),
+      ['<c-k>'] = cmp.mapping.select_prev_item(),
+      ['<c-space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+      ['<cr>'] = cmp.mapping.confirm {select = true},
+    },
+    formatting = {
+      fields = {'abbr', 'kind', 'menu'},
+      format = function(entry, item)
+        item.kind = string.format('[%s', kind_icons[item.kind], item.kind)
+        item.menu = string.format('%s]', entry.source.name)
+        return item
+      end
+    },
+    sources = {
+    {name = 'nvim_lsp'},
+      {name = 'nvim_lua'},
+      {name = 'buffer'},
+      {name = 'path'},
+},
+confirm_opts = {
+  behavior = cmp.ConfirmBehavior.Replace,
+select = false,
+}
+}
+end
+
+local function setup_lsp_keymaps(bufnr)
+  local opts = {noremap = true, silent = true}
+  local keymap = vim.api.nvim_buf_set_keymap
+
+  -- local command = 'c'
+  -- local insert = 'i'
+  local normal = 'n'
+  -- local term = 't'
+  -- local visual = 'v'
+  -- local visual_block = 'x'
+
+  keymap(bufnr, normal, '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  keymap(bufnr, normal, '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  keymap(bufnr, normal, 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  keymap(bufnr, normal, '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+  keymap(bufnr, normal, ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+  keymap(bufnr, normal, 'gd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  keymap(bufnr, normal, 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  keymap(bufnr, normal, 'gl', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>', opts)
+  keymap(bufnr, normal, 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  keymap(bufnr, normal, 'gt', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+end
+
+local function setup_lsp_settings()
+  local lsp_installer = require('nvim-lsp-installer')
+
+  lsp_installer.on_server_ready(function(server)
+    local cmp_nvim_lsp = require('cmp_nvim_lsp')
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+    local opts = {
+      on_attach = function(_, bufnr) setup_lsp_keymaps(bufnr) end,
+      capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+    }
+
+    if server.name == 'sumneko_lua' then
+      opts.settings = {
+        Lua = {
+          diagnostics = {
+            globals = {'vim'}
+          }
+        }
+      }
+    end
+
+    server:setup(opts)
+  end)
+end
+
+local function install_lsp_servers()
+  local lsp_installer_servers = require('nvim-lsp-installer.servers')
+
+  local servers = {
+    'bashls',
+    'clangd',
+    'rust_analyzer',
+    'sumneko_lua',
+  }
+
+  for _, server_name in pairs(servers) do
+    local available, server = lsp_installer_servers.get_server(server_name)
+    if available and not server:is_installed() then
+      server:install()
+    end
+  end
+end
+
+local function setup_telescope()
+  local telescope = require('telescope')
+  local actions = require('telescope.actions')
+
+  telescope.setup {
+    defaults = {
+      mappings = {
+        i = {
+          ['<C-p>'] = actions.preview_scrolling_up,
+          ['<C-n>'] = actions.preview_scrolling_down,
+          ['<C-j>'] = actions.move_selection_next,
+          ['<C-k>'] = actions.move_selection_previous,
+        }
+      }
+    }
+  }
+
+  telescope.load_extension('fzf')
+end
+
+local function setup_treesitter()
+  local configs = require('nvim-treesitter.configs')
+
+  configs.setup {
+    ensure_installed = 'maintained',
+    sync_install = false,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    }
+  }
+end
+
+local function setup_filetree()
+  local filetree = require('nvim-tree')
+  filetree.setup({
+    open_on_setup = true,
+    hijack_cursor = true,
+    auto_close = true,
+    open_on_tab = true,
+    quit_on_open = true,
+    group_empty = true,
+    diagnostics = {
+      enable = true,
+    },
+    view = {
+      auto_resize = true,
+      hide_dotfiles = true,
+      side = 'left',
+      width = 30,
+    },
+    git = {
+      ignore = false,
+    },
+    filters = {
+      dotfiles = true,
+      custom = {'.git'},
+    },
+    show_icons = {
+      git = false,
+      folders = true,
+      files = true,
+      folder_arrows = true,
+    },
+  })
+
+  vim.g.nvim_tree_indent_markers = 1
+  vim.g.nvim_tree_group_empty = 1
+end
+
+local function setup_autocmds()
+  vim.cmd [[
+    augroup general_settings
+    autocmd!
+    autocmd BufRead,BufNewFile README setlocal filetype=markdown
+    autocmd FileType gitcommit,hgcommit setlocal spell textwidth=72
+    autocmd FileType markdown,vimwiki setlocal spell comments+=b:>
+    augroup end
+  ]]
+
+  -- resize buffesr on vim window resize
+  vim.cmd [[
+    augroup resize_buffers
+    autocmd!
+    autocmd VimResized * tabdo wincmd =
+    augroup end
+  ]]
+
+  -- load vim configuration on file save
+  vim.cmd [[
+    augroup vim_configuration
+    autocmd!
+    autocmd BufWritePost init.lua source ~/.config/nvim/init.lua
+    augroup end
+  ]]
+
+  -- return to the same line when you reopen a file
+  vim.cmd [[
+    augroup line_return
+    autocmd!
+    autocmd BufReadPost *
+        \ if line("'\"") > 0 && line("'\"") <= line('$') |
+        \     execute 'normal! g`"zvzz' |
+        \ endif
+    augroup end
+  ]]
+
+  -- apply xresources file on save
+  vim.cmd [[
+    augroup xresources
+    autocmd!
+    autocmd BufWritePost *Xresources,*Xdefaults !xrdb %
+    augroup end
+  ]]
+
+  -- automatically delete all trailing whitespace and newlines at end of file on save
+  vim.cmd [[
+    augroup trailing_whitespace
+    autocmd!
+    autocmd BufWritePre * %s/\s\+$//e
+    autocmd BufWritepre * %s/\n\+\%$//e
+    augroup end
+  ]]
+
+  -- add formats for speeddating plugin
+  vim.cmd [[
+    augroup speeddating
+    autocmd!
+    autocmd VimEnter * SpeedDatingFormat %A, %B %d, %Y
+    autocmd VimEnter * SpeedDatingFormat %B %d, %Y
+    augroup end
+  ]]
+
+  -- format code on save
+  vim.cmd [[
+    augroup formatting
+    autocmd!
+    autocmd BufWritePre *.h,*.cc lua vim.lsp.buf.formatting_sync()
+    augroup end
+  ]]
+end
+
+local function main()
+  set_options()
+  set_keymaps()
+  install_plugins()
+  setup_completions()
+  setup_lsp_settings()
+  install_lsp_servers()
+  setup_telescope()
+  setup_treesitter()
+  setup_filetree()
+  setup_autocmds()
+
+  vim.cmd('colorscheme base16-default-dark')
+
+  if at_work() then
+    vim.cmd [[
+      source ~/code/fuchsia/scripts/vim/fuchsia.vim
+    ]]
+  end
+end
+
+main()
+
+-- we don't need these in neovim?
+-- the following lines should always be last
+-- filetype plugin indent on
+-- syntax on
