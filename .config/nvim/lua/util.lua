f06ac598bbebbfbda916a218188a9b40a4d10ad5
Faraaz Sareshwala <faraazs@gmail.com>
2025-02-13 01:31:29 -0800

    vim: switch to lazyvim

---
+++ .config/nvim/lua/util.lua
@@ -0,0 +1,40 @@
+local util = {}
+
+function string.startswith(haystack, needle)
+  return string.sub(haystack, 0, string.len(needle)) == needle
+end
+
+function string.endswith(haystack, suffix)
+  return string.sub(haystack, -#suffix) == suffix
+end
+
+function string.trim(str)
+  return string.gsub(str, "%s+", "")
+end
+
+function util.get_hostname()
+  local f = io.popen("/bin/hostname")
+  if f == nil then
+    return "unknown"
+  end
+
+  local value = f:read("*a")
+  f:close()
+  return string.gsub(value, "\n$", "")
+end
+
+function util.at_work()
+  local hostname = util.get_hostname()
+
+  if string.startswith(hostname, "fsareshwala-office") then
+    return true
+  elseif string.startswith(hostname, "fsareshwala-cloudtop") then
+    return true
+  elseif string.startswith(hostname, "fsareshwala-macbookpro") then
+    return true
+  end
+
+  return false
+end
+
+return util
