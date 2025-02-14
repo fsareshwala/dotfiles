1e8b8a2a90d8c3ffeb0c2183f80848cdcae092d2
Faraaz Sareshwala <faraazs@gmail.com>
2025-02-14 07:02:54 -0800

    vim: set clangd keymap only when the clangd server attaches

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,16 +3,16 @@ return {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
-        clangd = {},
+        clangd = {
+          keys = {
+            { '<leader>ce', '<cmd>ClangdSwitchSourceHeader<cr>', desc = 'Toggle source/header' },
+          },
+        },
         bashls = {},
         gopls = {},
         rust_analyzer = {},
         taplo = {},
       },
-    },
-    keys = {
-      { '<leader>ce', '<cmd>ClangdSwitchSourceHeader<cr>', desc = 'Toggle source/header' },
-      { 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', desc = 'Diagnostic help' },
     },
   },
 }
