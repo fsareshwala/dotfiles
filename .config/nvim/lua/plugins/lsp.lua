cf3bd6935f17fd46eb6ed3d9282481515ab0e040
Faraaz Sareshwala <faraazs@gmail.com>
2025-02-18 11:01:27 -0800

    vim: replace clangd cmd when working in pigweed

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,28 @@
+local cmd = { 'clangd' }
+
+if string.find(vim.fn.getcwd(), 'pigweed') then
+  local home = os.getenv('HOME')
+  local clangd = home .. '/code/pigweed/environment/cipd/packages/pigweed/bin/clangd'
+  cmd = {
+    clangd,
+    '--compile-commands-dir=' .. home .. '/code/pigweed/.pw_ide/.stable',
+    '--background-index',
+    '--clang-tidy',
+    '--query-driver='
+      .. home
+      .. '/code/pigweed/environment/cipd/packages/pigweed/bin/*,'
+      .. home
+      .. '/code/pigweed/environment/cipd/packages/arm/bin/*',
+  }
+end
+
 return {
   {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
         clangd = {
+          cmd = cmd,
           keys = {
             { '<leader>ce', '<cmd>ClangdSwitchSourceHeader<cr>', desc = 'Toggle source/header' },
           },
