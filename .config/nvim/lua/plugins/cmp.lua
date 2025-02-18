0fcbf94990868b4f66ede96fc64f88b065858596
Faraaz Sareshwala <faraazs@gmail.com>
2025-02-18 10:41:31 -0800

    vim: add borders to completion windows

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,10 @@
+return {
+  'saghen/blink.cmp',
+  opts = {
+    completion = {
+      menu = { border = 'single' },
+      documentation = { window = { border = 'single' } },
+    },
+    signature = { window = { border = 'single' } },
+  },
+}
