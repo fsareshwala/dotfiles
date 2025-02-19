a4fd7d028712d1c5122d485003fcef7a8f531595
Faraaz Sareshwala <faraazs@gmail.com>
2025-02-18 23:37:19 -0800

    vim: disable inlay hints

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -20,6 +20,9 @@ return {
   {
     'neovim/nvim-lspconfig',
     opts = {
+      inlay_hints = {
+        enabled = false,
+      },
       servers = {
         clangd = {
           cmd = clangd_cmd,
