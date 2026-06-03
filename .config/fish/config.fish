caec5a6938316d5355dfafe3faa935abed6e5975
Faraaz Sareshwala <faraazs@gmail.com>
2026-06-03 12:32:49 -0700

    fish: fix path building

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,6 +24,37 @@ end
 set -gx FZF_DEFAULT_COMMAND "rg --files --no-ignore-vcs --hidden --color=never *"
 set -gx FZF_DEFAULT_OPTS '--height 20% --reverse'
 
+# Path building
+function set_path --on-event fish_prompt --description 'Regenerate path completely dynamically'
+    # Pigweed overrides and handles its own path environments via source activate.sh
+    if in_pigweed
+        return
+    end
+
+    set -l target_path \
+        $HOME/prefix/bin \
+        $GOPATH/bin \
+        /usr/local/sbin \
+        /usr/local/bin \
+        /usr/sbin \
+        /usr/bin \
+        /sbin \
+        /bin
+
+    if in_fuchsia
+        set -a target_path $HOME/code/fuchsia/.jiri_root/bin $HOME/code/fuchsia/scripts
+    else
+        # Fuchsia doesn't like the current directory in the path
+        set -a target_path .
+    end
+
+    if is_mac_os
+        set -a target_path /opt/homebrew/bin
+    end
+
+    set -gx PATH $target_path
+end
+
 # Aliases
 alias -- .........='cd ../../../../../../../..'
 alias -- ........='cd ../../../../../../..'
