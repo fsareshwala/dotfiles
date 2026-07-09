ed65ed306321755f723c775086abdd7f17b5d8a6
Faraaz Sareshwala <faraazs@gmail.com>
2026-07-09 04:34:43 +0000

    fish: automatically determine tracking branch in gromx

--- .config/fish/functions/gromx.fish
+++ .config/fish/functions/gromx.fish
@@ -1,3 +1,4 @@
 function gromx --description 'Run a command on every commit since origin/master'
-    git rebase --exec "$argv" origin/master
+  set -l tracking_branch (git rev-parse --abbrev-ref @{upstream})
+  git rebase --exec "$argv" $tracking_branch
 end
