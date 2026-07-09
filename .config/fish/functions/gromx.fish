dfe1592b130d900fe486e752d28cf2764ed08069
Faraaz Sareshwala <faraazs@gmail.com>
2026-07-09 04:39:07 +0000

    fish: automatically add changed files in gromx

--- .config/fish/functions/gromx.fish
+++ .config/fish/functions/gromx.fish
@@ -1,4 +1,4 @@
 function gromx --description 'Run a command on every commit since origin/master'
   set -l tracking_branch (git rev-parse --abbrev-ref @{upstream})
-  git rebase --exec "$argv" $tracking_branch
+  git rebase --exec "$argv && git commit -a --amend --no-edit" $tracking_branch
 end
