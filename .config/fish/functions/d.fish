ae0dcbe98e59fece27731368a97bd0a93ff98c74
Faraaz Sareshwala <faraazs@gmail.com>
2026-07-09 04:55:16 +0000

    fish: simplify the d and st functions

--- .config/fish/functions/d.fish
+++ .config/fish/functions/d.fish
@@ -1,11 +1,7 @@
 function d
-    if at_work
-        if in_google3
-            hg diff $argv
-        else
-            git diff $argv
-        end
-    else
-        git diff
-    end
+  if in_google3
+    hg diff $argv
+  else
+    git diff $argv
+  end
 end
--- .config/fish/functions/st.fish
+++ .config/fish/functions/st.fish
@@ -1,11 +1,7 @@
 function st
-    if at_work
-        if in_google3
-            hg status $argv
-        else
-            git status $argv
-        end
-    else
-        git status
-    end
+  if in_google3
+    hg status $argv
+  else
+    git status $argv
+  end
 end
