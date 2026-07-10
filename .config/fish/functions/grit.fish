7f371d0bda5cc234dea36d017dd7317f5a766330
Faraaz Sareshwala <faraazs@gmail.com>
2026-07-10 04:58:55 +0000

    fish: add autostash to grit

--- .config/fish/functions/grit.fish
+++ .config/fish/functions/grit.fish
@@ -1,4 +1,4 @@
 function grit --description 'Interactively rebase commits from the tracking branch'
   set -l tracking_branch (git rev-parse --abbrev-ref @{upstream})
-  git rebase -i $tracking_branch
+  git rebase -i --autostash $tracking_branch
 end
