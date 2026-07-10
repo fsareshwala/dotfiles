8219544d4a13791e9b5912f515e6ef7be81197c2
Faraaz Sareshwala <faraazs@gmail.com>
2026-07-10 18:58:43 +0000

    fish: rename gromx to grx

--- .config/fish/functions/grx.fish
+++ .config/fish/functions/grx.fish
@@ -1,4 +1,4 @@
-function gromx --description 'Run a command on every commit since origin/master'
+function grx --description 'Run a command on every commit since the tracking branch'
   set -l tracking_branch (git rev-parse --abbrev-ref @{upstream})
   git rebase --exec "$argv && git commit -a --amend --no-edit" $tracking_branch
 end
